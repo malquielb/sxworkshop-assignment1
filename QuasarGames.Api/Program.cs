10291ac65d0c44b6671918f3523836616ebcbdf1
Malquiel B. H <malquielbeltreh@hotmail.com>
2022-05-09 11:33:25 -0400

    Logger Added

--- QuasarGames.Api/Program.cs
+++ QuasarGames.Api/Program.cs
@@ -1,5 +1,6 @@
 using QuasarGames.Application;
 using QuasarGames.Persistence;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+
+builder.Logging.AddSerilog();
 
 var app = builder.Build();
 
